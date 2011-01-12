c3da31e89d6cd23b4195ad631f717ae9d6610916
alanw <alanwright.atex@googlemail.com>
2011-01-12 14:01:40 +0000

    Update file headers with new year

--- include/ChecksumIndexInput.h
+++ include/ChecksumIndexInput.h
@@ -1,5 +1,5 @@
 /////////////////////////////////////////////////////////////////////////////
-// Copyright (c) 2009-2010 Alan Wright. All rights reserved.
+// Copyright (c) 2009-2011 Alan Wright. All rights reserved.
 // Distributable under the terms of either the Apache License (Version 2.0)
 // or the GNU Lesser General Public License.
 /////////////////////////////////////////////////////////////////////////////
