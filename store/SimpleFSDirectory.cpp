582f007874846d33ddbb750e967fb62b869ef65d
alanw <alanwright.atex@googlemail.com>
2011-01-12 13:09:59 +0000

    InputFile shouldn't be opening files for write
    (manual merge from ustramooner)

--- store/SimpleFSDirectory.cpp
+++ store/SimpleFSDirectory.cpp
@@ -41,7 +41,7 @@ namespace Lucene
     
     InputFile::InputFile(const String& path)
     {
-        file = newInstance<std::ifstream>(StringUtils::toUTF8(path).c_str(), std::ios::binary | std::ios::in | std::ios::out);
+        file = newInstance<std::ifstream>(StringUtils::toUTF8(path).c_str(), std::ios::binary | std::ios::in );
         if (!file->is_open())
             boost::throw_exception(FileNotFoundException(path));
         position = 0;
