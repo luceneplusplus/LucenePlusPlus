c5ed014df578f2be4c48ac8e6f09d6450d1a2a03
rezso <rezso@example.com>
2014-05-19 09:23:35 +0100

    lucene++ .pc files fix

--- liblucene++-contrib.pc.cmake
+++ liblucene++-contrib.pc.cmake
@@ -1,14 +1,13 @@
 prefix=@CMAKE_INSTALL_PREFIX@
 exec_prefix=${prefix}/bin
-libdir=@LIB_DESTINATION@
+libdir=${prefix}/@LIB_DESTINATION@
 includedir=${prefix}/include/lucene++
 lib=lucene++-contrib
 
 Name: liblucene++-contrib
 Description: Contributions for Lucene++ - a C++ search engine, ported from the popular Apache Lucene
 Version: @lucene++_VERSION@
-Libs: -L@LIB_DESTINATION@/ -l${lib}
+Libs: -L${prefix}/@LIB_DESTINATION@ -l${lib}
 Cflags: -I${includedir}
-Requires: liblucene++=@lucene++_VERSION@
-~
+Requires: liblucene++ = @lucene++_VERSION@
 
--- liblucene++.pc.cmake
+++ liblucene++.pc.cmake
@@ -1,13 +1,12 @@
 prefix=@CMAKE_INSTALL_PREFIX@
 exec_prefix=${prefix}/bin
-libdir=@LIB_DESTINATION@
+libdir=${prefix}/@LIB_DESTINATION@
 includedir=${prefix}/include/lucene++
 lib=lucene++
 
 Name: liblucene++
 Description: Lucene++ - a C++ search engine, ported from the popular Apache Lucene
 Version: @lucene++_VERSION@
-Libs: -L@LIB_DESTINATION@ -l${lib}
+Libs: -L${prefix}/@LIB_DESTINATION@ -l${lib}
 Cflags: -I${includedir}
-~
 
