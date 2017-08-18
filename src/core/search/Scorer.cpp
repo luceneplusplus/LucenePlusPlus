0e94cff7381de0fa63811ea3a69f21e1135d784c
sravan rekula <sravan@thinair.com>
2017-08-18 12:50:26 -0700

    implement scorer visitor

--- src/core/search/Scorer.cpp
+++ src/core/search/Scorer.cpp
@@ -9,34 +9,62 @@
 #include "Collector.h"
 
 namespace Lucene {
-
-Scorer::Scorer(const SimilarityPtr& similarity) {
-    this->similarity = similarity;
-}
-
-Scorer::~Scorer() {
-}
-
-SimilarityPtr Scorer::getSimilarity() {
-    return similarity;
-}
-
-void Scorer::score(const CollectorPtr& collector) {
-    collector->setScorer(shared_from_this());
-    int32_t doc;
-    while ((doc = nextDoc()) != NO_MORE_DOCS) {
-        collector->collect(doc);
+    
+    Scorer::Scorer(const SimilarityPtr& similarity) {
+        this->similarity = similarity;
     }
-}
-
-bool Scorer::score(const CollectorPtr& collector, int32_t max, int32_t firstDocID) {
-    collector->setScorer(shared_from_this());
-    int32_t doc = firstDocID;
-    while (doc < max) {
-        collector->collect(doc);
-        doc = nextDoc();
+    
+    Scorer::Scorer(const WeightPtr& weight) {
+        this->weight = weight;
     }
-    return (doc != NO_MORE_DOCS);
-}
-
+    
+    Scorer::~Scorer() {
+    }
+    
+    SimilarityPtr Scorer::getSimilarity() {
+        return similarity;
+    }
+    
+    void Scorer::score(const CollectorPtr& collector) {
+        collector->setScorer(shared_from_this());
+        int32_t doc;
+        while ((doc = nextDoc()) != NO_MORE_DOCS) {
+            collector->collect(doc);
+        }
+    }
+    
+    bool Scorer::score(const CollectorPtr& collector, int32_t max, int32_t firstDocID) {
+        collector->setScorer(shared_from_this());
+        int32_t doc = firstDocID;
+        while (doc < max) {
+            collector->collect(doc);
+            doc = nextDoc();
+        }
+        return (doc != NO_MORE_DOCS);
+    }
+    
+    void Scorer::visitSubScorers(QueryPtr parent, BooleanClause::Occur relationship,
+                                 ScorerVisitor *visitor){
+        QueryPtr q = weight->getQuery();
+        switch (relationship) {
+            case BooleanClause::MUST:
+            visitor->visitRequired(parent, q, shared_from_this());
+            break;
+            case BooleanClause::MUST_NOT:
+            visitor->visitProhibited(parent, q, shared_from_this());
+            break;
+            case BooleanClause::SHOULD:
+            visitor->visitOptional(parent, q, shared_from_this());
+            break;
+        }
+    }
+    
+    void Scorer::visitScorers(ScorerVisitor *visitor) {
+        boost::shared_ptr<Query> s_obj;
+        
+        visitSubScorers(s_obj, BooleanClause::MUST/*must id default*/, visitor);
+    }
+    
+    
+    
 }
